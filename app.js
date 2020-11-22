119fd02a595870e37d541520a477c2ef3763c3bd
Ivan <ivanrj7j@gmail.com>
2020-11-22 17:26:28 +0530

    dont know what i did

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var search_toggler = document.getElementById('toggler');
 var search_icon = document.querySelector('.search-btn');
-var navitems = document.querySelector('.navitem');
+var navitems = document.querySelector('#navitem');
+var navtoggle = document.querySelector('#barsline');
 
-search_icon.addEventListener('click', function(){
+search_icon.addEventListener('click', function () {
     search_icon.style.display = 'none';
     search.classList.remove('d-none');
     search.focus()
@@ -11,8 +12,34 @@ search_icon.addEventListener('click', function(){
 
 var search = document.getElementById('search');
 
-search.addEventListener('focusout', function(){
+search.addEventListener('focusout', function () {
     search.classList.add('d-none');
     search_icon.style.display = 'block';
     navitems.style.left = '30%';
+});
+
+var toggled = false;
+navtoggle.addEventListener('click', function () {
+    if (toggled) {
+        navtoggle.classList.add('fa-bars');
+        navtoggle.classList.remove('fa-times');
+        toggled = false;
+        navitems.classList.remove('navitem-t');
+        navitems.classList.add('navitem')
+    } else {
+        navtoggle.classList.remove('fa-bars');
+        navtoggle.classList.add('fa-times');
+        toggled = true;
+        navitems.classList.remove('navitem');
+        navitems.classList.add('navitem-t')
+    }
+});
+
+window.addEventListener('resize', () => {
+
+    navitems.classList.remove('navitem-t');
+    navitems.classList.add('navitem');
+    navtoggle.classList.add('fa-bars');
+    navtoggle.classList.remove('fa-times');
+
 });
