13102403cb551d807c60b2d85ce71b6fd39efa99
Ivan <ivanrj7j@gmail.com>
2020-11-22 20:05:27 +0530

    need to work on like and dislike btn

--- app.js
+++ app.js
@@ -44,4 +44,83 @@ window.addEventListener('resize', () => {
 
 });
 
-// Navbar JavaScript top 
+// Navbar JavaScript top
+
+// content start 
+var like = document.querySelectorAll('.like');
+var dislike = document.querySelectorAll('.dislike');
+var share = document.querySelectorAll('.share');
+
+function like_it(post) {
+
+}
+
+
+function unlike(post) {
+
+}
+
+
+function dislike_it(post) {
+
+}
+
+
+function undislike(post) {
+
+}
+
+for (let i = 0; i < like.length; i++) {
+    const like_btn = like[i];
+    const post_id = like_btn.parentElement.parentElement.parentElement.parentElement.parentElement.classList[1];
+
+    like_btn.parentElement.addEventListener('click', function () {
+        like_btn.click();
+    });
+    var liked = false;
+    like_btn.addEventListener('click', function () {
+        var dislike = Boolean('disliked' == like_btn.parentElement.parentElement.childNodes[3].childNodes[0].classList[1] || 'disliked' in disbtn.parentElement.parentElement.childNodes[3].childNodes[0].classList);
+        var disbtn = like_btn.parentElement.parentElement.childNodes[3].childNodes[0];
+        
+        if (liked) {
+            like_btn.classList.remove('liked');
+            liked = false;
+            unlike(post_id);
+        } else {
+            like_btn.classList.add('liked');
+            liked = true;
+            like_it(post_id);
+            if (dislike) {
+                disbtn.click();
+            }
+        }
+    });
+}
+
+for (let index = 0; index < dislike.length; index++) {
+    const disbtn = dislike[index];
+    const post_id = disbtn.parentElement.parentElement.parentElement.parentElement.parentElement.classList[1];
+    var liked = Boolean('liked' == disbtn.parentElement.parentElement.childNodes[1].childNodes[0].classList[1] || 'liked' in disbtn.parentElement.parentElement.childNodes[1].childNodes[0].classList);
+    var likebtn = disbtn.parentElement.parentElement.childNodes[1].childNodes[0];
+    var disliked = false;
+
+    disbtn.parentElement.addEventListener('click', function () {
+        disbtn.click();
+    });
+
+    disbtn.addEventListener('click', function () {
+        if (disliked) {
+            disbtn.classList.remove('disliked');
+            undislike(post_id)
+            disliked = false;
+        } else {
+            disbtn.classList.add('disliked');
+            dislike_it(post_id);
+            disliked = true;
+            if (liked) {
+                likebtn.click();
+            }
+        }
+    });
+}
+// content end
