84f57acb15134da4cd56e67910498d072a023d70
Ivan <ivanrj7j@gmail.com>
2020-11-22 12:49:00 +0530

    added the navbar toggler

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-var search_toggler = document.getElementById('toggler')
+var search_toggler = document.getElementById('toggler');
+var search_icon = document.querySelector('.search-btn');
+var navitems = document.querySelector('.navitem');
+
+search_icon.addEventListener('click', function(){
+    search_icon.style.display = 'none';
+    search.classList.remove('d-none');
+    search.focus()
+    navitems.style.left = '17.2%';
+});
+
+var search = document.getElementById('search');
+
+search.addEventListener('focusout', function(){
+    search.classList.add('d-none');
+    search_icon.style.display = 'block';
+    navitems.style.left = '30%';
+});
