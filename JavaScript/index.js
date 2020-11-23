80d81d40a4838f08d6f7a96d0b0ea6ad457c60d2
Ivan <ivanrj7j@gmail.com>
2020-11-23 13:44:42 +0530

    video player ready

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -35,7 +35,7 @@ function actions(like_btn, value, obj) {
     var liked = Boolean('liked' in like_btn.classList || 'liked' == like_btn.classList[1]);
     var disliked = Boolean('disliked' in like_btn.classList || 'disliked' == like_btn.classList[1]);
     like_btn.addEventListener('click', function () {
-        
+
         if (liked) {
             like_btn.classList.remove('liked');
             liked = false;
@@ -79,14 +79,14 @@ function change_width(items, value) {
     var video = items.parentElement.querySelector('video');
     var playpause = items.querySelector('.playpause');
     var buffer_range = items.querySelector('.buffered')
-    juice.addEventListener('mousedown', (e)=>{
+    juice.addEventListener('mousedown', (e) => {
         var rect = e.target.getBoundingClientRect();
         var x = e.clientX - rect.left;
         var div_width = juice.offsetWidth;
         var width_percent = x / div_width * 100;
         video.currentTime = video.duration * (width_percent / 100);
     });
-    juice.addEventListener('mouseup', (e)=>{
+    juice.addEventListener('mouseup', (e) => {
         var rect = e.target.getBoundingClientRect();
         var x = e.clientX - rect.left;
         var div_width = juice.offsetWidth;
@@ -94,18 +94,20 @@ function change_width(items, value) {
         video.currentTime = video.duration * (width_percent / 100);
     });
     var icon = playpause.querySelector('.fa');
-    playpause.addEventListener('click', ()=>{
+    playpause.addEventListener('click', () => {
         if (video.paused) {
             video.play();
             icon.classList.remove('fa-play')
             icon.classList.add('fa-pause')
-        }else{
+        } else {
             video.pause();
             icon.classList.remove('fa-pause');
             icon.classList.add('fa-play');
         }
     });
-    video.addEventListener('timeupdate', ()=>{
+    var timestamp = items.querySelector('.timestamp');
+
+    video.addEventListener('timeupdate', () => {
         var percent = (video.currentTime / video.duration) * 100;
         juice_con.style.width = percent + "%";
         var buffered = video.buffered.end(0);
@@ -116,6 +118,30 @@ function change_width(items, value) {
             icon.classList.remove('fa-play');
             icon.classList.add('fa-play');
         }
+        var durmins = Math.floor(video.duration / 60);
+        var dursec = Math.floor(video.duration - durmins * 60);
+        var curmins = Math.floor(video.currentTime / 60);
+        var cursec = Math.floor(video.currentTime - curmins * 60);
+        if (durmins < 10) {
+            durmins = "0" + durmins
+        }
+
+        if (dursec < 10) {
+            dursec = "0" + dursec
+        }
+        if (curmins < 10) {
+            curmins = "0" + curmins
+        }
+
+        if (cursec < 10) {
+            cursec = "0" + cursec
+        }
+        timestamp.innerHTML = curmins + ':' + cursec + "/" + durmins + ":" + dursec;
+
+        
+    });
+    video.addEventListener('click', ()=>{
+        playpause.click();
     });
 }
 
