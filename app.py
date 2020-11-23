99ee2a65bcf012228d0e4631f26e74fbe0ef1c2c
Ivan <ivanrj7j@gmail.com>
2020-11-23 21:57:24 +0530

    fixed somethins

--- app.py
+++ app.py
@@ -10,11 +10,21 @@ title = general_settings['title']
 # importing some required variables z
 app = Flask(__name__)
 
-# initialising important variable 
+# initialising important variable
+
+def login_required(func):
+    """
+    Checks if user is logged in
+    """
+    if 'email' in session and 'name' in session:
+        func
+    else:
+        return redirect('/login')
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', js='JavaScript/index.js', css='style/index.css')
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=5000, debug=True)
